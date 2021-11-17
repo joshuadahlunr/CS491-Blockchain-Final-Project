8ce99e1484324b4492d4dd1094640316afc54f15
Joshua Dahl <joshuadahl@nevada.unr.edu>
2021-11-17 12:42:56 -0800

    Graceful Terminiation
    Added a signal handler so that ctrl+c causes a graceful shutdown of the network and handshake listener.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <chrono>
 #include <array>
+#include <signal.h>
 #include <boost/uuid/uuid_io.hpp>
 #include <breep/network/tcp.hpp>
 #include <breep/util/serialization.hpp>
@@ -16,29 +17,55 @@
 #include "keys.hpp"
 #include "cryptopp/oids.h"
 
+bool handshakeThreadShouldRun = true;
+std::unique_ptr<breep::tcp::network> network;
+std::thread handshakeThread;
+
+// Function which handles cleaning up the program (used for normal termination and gracefully cleaning up when terminated)
+void shutdownProcedure(int signal){
+	// Clean up the handshake thread (if started)
+	if(handshakeThread.joinable()){
+		handshakeThreadShouldRun = false;
+		handshakeThread.detach();
+		std::cout << "Stopped handshake listener" << std::endl;
+	}
+
+	// Disconnect from the network (if connected)
+	if(network){
+		network->disconnect();
+		std::cout << "Disconnected from the network" << std::endl;
+	}
+
+	// Force quit the program (required when this is called from a signal)
+	std::exit(signal);
+}
+
 int main(int argc, char* argv[]) {
 	if (argc != 1 && argc != 2) {
 		std::cout << "Usage: " << argv[0] << " [<target ip>]" << std::endl;
 		return 1;
 	}
 
+	// Clean up the network connection and handshake thread if we are force shutdown
+	signal(SIGINT, shutdownProcedure);
+
 	// Create an IO service used by the handshake algorithm
 	boost::asio::io_service io_service;
 
 	// Find an open port for us to listen on and create a network listening on it
 	unsigned short localPort = determineLocalPort();
-	breep::tcp::network network(localPort);
+	network = std::make_unique<breep::tcp::network>(localPort);
 	// Create a network synched tangle
-	NetworkedTangle t(network);
+	NetworkedTangle t(*network);
 
 	// TODO: Need a mechanism for saving generated keys and loading them
 	t.setKeyPair( std::make_shared<key::KeyPair>(key::generateKeyPair(CryptoPP::ASN1::secp160r1())) );
 
 	// Disabling all logs (set to 'warning' by default).
-	network.set_log_level(breep::log_level::none);
+	network->set_log_level(breep::log_level::none);
 
 	// If we receive a class for which we don't have any listener (such as an int, for example), this will be called.
-	network.set_unlistened_type_listener([](breep::tcp::network&,const breep::tcp::peer&,breep::deserializer&,bool,uint64_t) -> void {
+	network->set_unlistened_type_listener([](breep::tcp::network&,const breep::tcp::peer&,breep::deserializer&,bool,uint64_t) -> void {
 		std::cout << "Unidentified message received!" << std::endl;
 	});
 
@@ -46,7 +73,7 @@ int main(int argc, char* argv[]) {
 	// Connect to the network
 	if (argc == 1) {
 		// runs the network in another thread.
-		network.awake();
+		network->awake();
 
 		std::cout << "Established a network on port " << localPort << std::endl;
 
@@ -64,36 +91,33 @@ int main(int argc, char* argv[]) {
 		// Find network connection (if we can't quickly find one ask for a manual port number)
 		boost::asio::ip::address address = boost::asio::ip::address::from_string(argv[1]);
 		unsigned short remotePort = handshake::determineRemotePort(io_service, address);
-		if(!network.connect(address, remotePort)){ // TODO: Hangs on invalid connection
+		if(!network->connect(address, remotePort)){ // TODO: Hangs on invalid connection
 			std::cout << "Failed to connect to the network" << std::endl;
 			return 2;
 		}
 
 		// Send our public key to the rest of the network
-		network.send_object(NetworkedTangle::PublicKeySyncRequest());
+		network->send_object(NetworkedTangle::PublicKeySyncRequest());
 		// Wait half a second
 		std::this_thread::sleep_for(std::chrono::milliseconds(500)); // TODO: Make this delay unessicary
 
 		std::cout << "Connected to the network (listening on port " << localPort << ")" << std::endl;
 
 		// If we are a client... ask the network for the tangle
-		network.send_object(NetworkedTangle::TangleSynchronizeRequest(t)); // TODO: Only force us to sync keys with people we actually communicate with
+		network->send_object(NetworkedTangle::TangleSynchronizeRequest(t)); // TODO: Only force us to sync keys with people we actually communicate with
 	}
 
 
 	// Find an open port for the handshake listener, and create a thread accepting handshakes
 	auto lp = determineLocalPort();
 	boost::asio::ip::tcp::acceptor acceptor(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), lp));
-	bool handshakeThreadShouldRun = true;
-	std::thread handshakeThread([&acceptor, &io_service, localPort, &handshakeThreadShouldRun](){
+	handshakeThread = std::thread([&acceptor, &io_service, localPort](){
 		while(handshakeThreadShouldRun)
 			handshake::acceptHandshakeConnection(acceptor, io_service, localPort);
 	});
 	std::cout << "Started handshake listener on port " << lp << std::endl;
 
 
-
-	// TODO: connection always fails when connecting to secondary client
 
 
 
@@ -106,7 +130,7 @@ int main(int argc, char* argv[]) {
 				std::vector<Transaction::Input> inputs;
 				inputs.emplace_back(*t.personalKeys, 100.0 + std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
 				std::vector<Transaction::Output> outputs;
-				outputs.push_back({t.peerKeys[network.peers().begin()->second.id()], 100.0});
+				outputs.push_back({t.peerKeys[network->peers().begin()->second.id()], 100.0});
 
 				t.add(TransactionNode::create(t.getTips(), inputs, outputs));
 			}
@@ -134,10 +158,6 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-	network.disconnect();
-	std::cout << "Disconnected from the network" << std::endl;
-
-	// Clean up the handshake thread
-	handshakeThreadShouldRun = false;
-	handshakeThread.detach();
+	// Clean up
+	shutdownProcedure(0);
 }
